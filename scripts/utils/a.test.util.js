f3aa620d5dc615ee83335c8b148ec08d17225281
Liil11 <ilhammunawarhanif18@gmail.com>
2026-09-17 09:51:47 +0700

    Remove submitWord function from a.test.util.js

--- scripts/utils/a.test.util.js
+++ scripts/utils/a.test.util.js
@@ -1,9 +1,1 @@
-import { add } from "./wordle.util.js";
 
-
-export const submitWord = (string)=>{
-    const word = [...string.toUpperCase()];
-    for(let letter of word){
-        console.log(letter);
-    }
-}
